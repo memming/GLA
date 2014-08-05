2c378997d989bfb1ea977c6dd2e0974235892406
Memming Park <memming@utexas.edu>
2014-08-05 17:28:29 -0500

    Speed test now runs
    - nonlinear functions from ncclabcode
    - non-zaso version of the likelihood implemented

---
+++ code/cmd_speed_test_poisson_regression.m
@@ -0,0 +1,64 @@
+% We compare various implementation of Poisson regressions
+
+addpath('zaso');
+addpath('nlfuns');
+
+%% Generate fake data
+m = 100; % # of variables
+N = 100000;
+wTrue = sin((1:m)'/10)/10;
+x = randn(m, N);
+bTrue = -3;
+lambda = exp(wTrue' * x + bTrue);
+y = poissrnd(lambda);
+fnlin = @expfun;
+w0 = zeros(numel(wTrue)+1, 1);
+fprintf('Mean [%f], Sparsity [%f]\n', mean(y), nnz(y)/N);
+
+%%%%%%%%%%%%%%%%%%%%%%% Regression Time %%%%%%%%%%%%%%%%%%%%%%%
+
+%% zaso version
+% TODO why the hell do I need to transpose????
+disp('>>> zaso, fminunc');
+tic;
+zaso = encapsulateRaw([x; ones(1,N)]', y'); 
+
+optimOpts = optimoptions(@fminunc, ...
+    'GradObj', 'on', 'Hessian', 'on', 'Display', 'off');
+[wOpt1, nLL1, exitflag1, output1] = fminunc(...
+    @(w) neglogli_poissGLM_zaso(w, zaso, fnlin), w0, optimOpts);
+t1 = toc;
+
+fprintf('[%f sec] # function call [%d], # of iterations [%d]\n', ...
+    t1, output1.funcCount, output1.iterations);
+
+%% minFunc, Newton
+disp('>>> zaso, minFunc, Newton');
+if exist('minFunc') ~= 2
+    error('Need minFunc for testing!');
+end
+% addpath('~/pillowlab/lib/minFunc_2012/minFunc/');
+tic;
+zaso = encapsulateRaw([x; ones(1,N)]', y'); 
+
+opts1 = struct('maxFunEvals', 100, 'Method', 'newton', 'display', 'none');
+[wOpt2, nLL2, exitflag2, output2] = minFunc(...
+    @(w)(neglogli_poissGLM_zaso(w, zaso, fnlin)), w0, opts1);
+
+t2 = toc;
+
+fprintf('[%f sec] # function call [%d], # of iterations [%d]\n', ...
+    t2, output2.funcCount, output2.iterations);
+
+%% Non-zaso version
+disp('>>> no-zaso, fminunc');
+tic;
+optimOpts = optimoptions(@fminunc, ...
+    'GradObj', 'on', 'Hessian', 'on', 'Display', 'off');
+x1 = [x; ones(1,N)]';
+[wOpt3, nLL3, exitflag3, output3] = fminunc(...
+    @(w) neglogli_poissGLM(x1, y', w, fnlin), w0, optimOpts);
+t3 = toc;
+
+fprintf('[%f sec] # function call [%d], # of iterations [%d]\n', ...
+    t3, output3.funcCount, output3.iterations);
--- code/neglogli_poissGLM.m
+++ code/neglogli_poissGLM.m
@@ -1,117 +1,53 @@
-function [L,dL,ddL] = neglogli_poissGLM_zaso(wts, zaso, fnlin)
-% Negative log-likelihood for Poisson GLM using ZASO
-% [L,dL,ddL] = neglogli_poissGLM(wts, zaso, Y)
-%
-% Compute negative log-likelihood of data under Poisson regression model,
-% plus gradient and Hessian
-%
-% INPUT:
-%   wts: (m x 1) regression weights
-%   zaso: Zhe Abstract Sample Object
+function [L, dL, ddL] = neglogli_poissGLM(x, y, wts, fnlin)
+% Negative log-likelihood for Poisson GLM
+% [L, dL, ddL] = neglogli_poissGLM(x, y, wts, fnlin)
+% 
+% INPUT
+%   x: (N x m) independent variable
+%   y: (N x 1) dependent variable
+%   wts: (m x 1) weights
 %   fnlin: @(x)->(f,df,ddf) func handle for nonlinearity 
 %	   (must return f, df and ddf; derivatives of f)
 %
-% OUTPUT:
+% OUTPUT
 %   L: (1 x 1) negative log-likelihood
 %  dL: (m x 1) gradient of L wrt weights
 % ddL: (m x m) Hessian of L wrt weights
 %
-% Example usage:
-%   fminunc(@(w) neglogli_poissGLM_zaso(w, zaso, fnlin), wInit)
+% See Also: neglogli_poissGLM_zaso
 
 m = numel(wts);
+xproj = x*wts;
 
 if isequal(fnlin, @expfun) || isequal(fnlin, @exp) || strcmpi(exp, 'exp')
-    % fast special computation for exponential nonlinearity
-    % Note that this trick of comparing function handles doesn't work for ones
-    % that are loaded from MAT files.
+    f = exp(xproj);
+
     switch nargout
 	case 1
-	    L = zasoFxysum(zaso, ...
-		@(x,y) neglogli_poissGLM_sub_exp(x, y, wts, 1));
+	    L = -y' * xproj + sum(f);
 	case 2
-	    r = zasoFxysum(zaso, ...
-		@(x,y) neglogli_poissGLM_sub_exp(x, y, wts, 2));
-	    L = r(1);
-	    dL = r(2);
-	otherwise
-	    r = zasoFxysum(zaso, ...
-		@(x,y) neglogli_poissGLM_sub_exp(x, y, wts, 3));
-	    L = r(1);
-	    dL = r(1 + (1:m));
-	    ddL = reshape(r(m+2:end), m, m);
+	    L = -y' * xproj + sum(f);
+	    dL = x' * (f - y);
+	case 3
+	    L = -y' * xproj + sum(f);
+	    dL = x' * (f - y);
+	    ddL = x' * bsxfun(@times, x, f);
     end
     return
 end
 
 switch nargout
     case 1
-        L = zasoFxysum(zaso, @(x,y) neglogli_poissGLM_sub(x, y, wts, fnlin, 1));
-    case 2
-        r = zasoFxysum(zaso, @(x,y) neglogli_poissGLM_sub(x, y, wts, fnlin, 2));
-        L = r(1);
-        dL = r(2);
-    otherwise
-        r = zasoFxysum(zaso, @(x,y) neglogli_poissGLM_sub(x, y, wts, fnlin, 3));
-        L = r(1);
-        dL = r(1 + (1:m));
-        ddL = reshape(r(m+2:end), m, m);
-end
-
-end % neglogli_poissGLM
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function L = neglogli_poissGLM_sub(x, y, wts, fnlin, choice)
-% choice = 1: only likelihood
-%	   2: likelihood and gradient
-%	   3: likelihood, graidient, and Hessian
-
-m = numel(wts);
-xproj = x*wts;
-
-switch choice
-    case 1
         f = fnlin(xproj);
         L = -y'*log(f) + sum(f);
     case 2
         [f,df] = fnlin(xproj);
-        L = zeros(m+1, 1);
-        L(1) = -y'*log(f) + sum(f);
-        L(1 + (1:m)) = x'*((1 - y./f) .* df);
+        L = -y'*log(f) + sum(f);
+	dL = x'*((1 - y./f) .* df);
     case 3
         [f,df,ddf] = fnlin(xproj);
-        L = zeros(m^2+m+1, 1);
-        L(1) = -y'*log(f) + sum(f);
+        L = -y'*log(f) + sum(f);
         yf = y./f;
-        L(1 + (1:m)) = x'*((1 - yf) .* df);
-	H = bsxfun(@times,ddf.*(1-yf)+df.*(y./f.^2.*df) ,x)'*x;
-        L(m+2:end) = H(:);
+	dL = x'*((1 - yf) .* df);
+	ddL = bsxfun(@times,ddf.*(1-yf)+df.*(y./f.^2.*df) ,x)'*x;
 end
-end % neglogli_poissGLM_sub
-
-function L = neglogli_poissGLM_sub_exp(x, y, wts, choice)
-% choice = 1: only likelihood
-%	   2: likelihood and gradient
-%	   3: likelihood, graidient, and Hessian
-%
-% special case for exponential nonlinearity (the CANONICAL LINK)
-
-m = numel(wts);
-xproj = x*wts;
-f = exp(xproj);
-
-switch choice
-    case 1
-        L = -y'*xproj + sum(f);
-    case 2
-        L = zeros(m+1, 1);
-        L(1) = -y'*xproj + sum(f);
-        L(1 + (1:m)) = x'*(f - y);
-    case 3
-        L = zeros(m^2+m+1, 1);
-        L(1) = -y'*xproj + sum(f);
-        L(1 + (1:m)) = x'*(f - y);
-	H = x'*bsxfun(@times,x,ddf);
-        L(m+2:end) = H(:);
-end
-end % neglogli_poissGLM_sub_exp
